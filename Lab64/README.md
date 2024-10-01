bde2a5f0b879f67226e2d226095983e7c4f0b535
José Nilson Praxedes Filho <jnpf.2013@hotmail.com>
2024-10-01 17:03:16 -0300

    correções

---
+++ Lab64/README.md
@@ -0,0 +1,7 @@
+1. Testar a página inicial:
+curl http://127.0.0.1:3000/
+2. Testar a página Sobre:
+curl http://127.0.0.1:3000/about
+3. Upload de arquivo:
+curl -X POST -F "file=@./public/about.html" http://localhost:3000/submit
+Ou então C:\Windows\System32\curl.exe -X POST -F "file=@./public/about.html" http://localhost:3000/upload
--- Lab64/server.js
+++ Lab64/server.js
@@ -1,18 +1,13 @@
+
 const express = require('express');
 const path = require('path');
-const fs = require('fs'); // Certifique-se de ter isso importado
 
 const app = express();
-const hostname = '127.0.0.1';
 const port = 3000;
 
 // Configura o Express para servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware para parsear o corpo das requisições
-app.use(express.urlencoded({ extended: true }));
-
-// Rota para a página inicial
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
@@ -21,29 +16,25 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
-
-// Rota para a página do formulário
+//Rota para a pagina formulario
 app.get('/formulario', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'formulario.html'));
 });
 
-// Rota para processar o formulário
-app.post('/submit', (req, res) => {
-    const { name, email, mensagem } = req.body;
-    const a = `username: ${name}, email: ${email}, mensagem: ${mensagem}`;
 
-    fs.writeFile(path.join(__dirname, 'data.txt'), a, err => {
-        if (err) {
-            console.error(err);
-            return res.send('Erro ao salvar os dados.');
-        }
-        res.send('Dados salvos com sucesso!');
+app.post('/submit', (req, res) =>{
+    let fileData = '';
+    req.on('data', chunk => {
+        fileData += chunk.toString();
     });
-});
-
-// Middleware para tratamento de erros 404
+    req.on('end', () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end('Upload simulado com sucesso!');
+    })
+})
 app.use((req, res) => {
-    res.status(404).send('Página não encontrada - ERRO 404');
+    res.status(404).send('Página não encontrada -ERRO 404)');
 });
 
 // Inicia o servidor
